873aa24b6d3649050092d6c273dd178f7cc97415
Ben Selleslagh <ben@dataframe.be>
2024-08-20 18:51:44 +0200

    Fixes #3

--- src/streamlit_app/view_sources.py
+++ src/streamlit_app/view_sources.py
@@ -5,19 +5,23 @@ import pandas as pd
 SOURCES_DF = pd.DataFrame(st.session_state.db.get_collection_metdata(st.session_state.project)).drop_duplicates()
 
 st.title("Sources")
-st.subheader("Statistics 📊")
-col1, col2, col3 = st.columns(3)
-col1.metric("Total Documents", len(SOURCES_DF))
-col2.metric("Total Sources", SOURCES_DF['source_type'].nunique())
-col3.metric("Amount of formats", SOURCES_DF['source_format'].nunique())
+
 
 
 
 
 st.subheader("Indexed Data")
 
-st.dataframe(SOURCES_DF[["url", "title", "source_type", "source_format", "language"]], 
-            width=800,
-            column_config={
-                        "url" : st.column_config.LinkColumn(),
-                    })
+if len(SOURCES_DF) == 0:
+    st.warning("No sources indexed yet.")
+else:
+    st.subheader("Statistics 📊")
+    col1, col2, col3 = st.columns(3)
+    col1.metric("Total Documents", len(SOURCES_DF))
+    col2.metric("Total Sources", SOURCES_DF['source_type'].nunique())
+    col3.metric("Amount of formats", SOURCES_DF['source_format'].nunique())
+    st.dataframe(SOURCES_DF[["url", "title", "source_type", "source_format", "language"]], 
+                width=800,
+                column_config={
+                            "url" : st.column_config.LinkColumn(),
+                        })
