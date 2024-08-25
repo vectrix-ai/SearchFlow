c06c534716830e38f8e0e427f95b8985f4ec1a60
Ben Selleslagh <ben@dataframe.be>
2024-08-25 13:45:37 +0200

    fixed #13

--- src/streamlit_app/scrape_data.py
+++ src/streamlit_app/scrape_data.py
@@ -27,6 +27,7 @@ with col1:
 
 st.subheader('Scrape job status')
 scrape_job_status = pd.DataFrame(st.session_state.db.get_scrape_status(project_name=st.session_state.project))
+scrape_job_status = scrape_job_status.dropna(subset=['base_url'])
 if len(scrape_job_status) > 0:
     st.dataframe(scrape_job_status, hide_index=True)
 else:
@@ -34,6 +35,21 @@ else:
 
 
 st.subheader('Links to confirm')
+
+webscraper = WebScraper(project_name=st.session_state.project)
+
+@st.dialog("Import a single webpage")
+def import_webpage():
+    url = st.text_input("URL")
+    if st.button("Import"):
+        webscraper.download_pages(urls=[url], project_name=st.session_state.project)
+        st.success("Webpage imported successfully")
+        st.rerun()
+
+if st.button("Import a webpage"):
+    import_webpage()
+
+
 links_to_confirm = pd.DataFrame(st.session_state.db.get_links_to_confirm(project_name=st.session_state.project))
 if len(links_to_confirm) > 0:
     links_to_confirm['Extract'] = True
@@ -50,7 +66,6 @@ if len(links_to_confirm) > 0:
     if st.button('Confirm page import'):
         selected_links = links_to_confirm[links_to_confirm['Extract'] == True]['url'].tolist()
         st.success('Sraping job submitted')
-        webscraper = WebScraper(project_name=st.session_state.project)
         webscraper.download_pages(urls=selected_links, project_name=st.session_state.project)
 else:
     st.warning("No links to confirm")
